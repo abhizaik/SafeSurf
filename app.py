62448194520bbddfea6648846d860a681ebb447c
incogGod <incoggod@proton.me>
2023-03-22 01:53:25 +0530

    added website preview option

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, render_template
+from bs4 import BeautifulSoup
+import requests
+from urllib.parse import urljoin
 import controller
 
 app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/result', methods=['POST'])
 def result():
@@ -13,8 +17,30 @@ def result():
     result = controller.main(url)
     output = result
 
-    return render_template('index.html', output=output)
+    return render_template('result.html', output=output)
 
+
+@app.route('/preview/<path:url>')
+def preview(url):
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # inject external resources into HTML
+        for link in soup.find_all('link'):
+            if link.get('href'):
+                link['href'] = urljoin(url, link['href'])
+        for script in soup.find_all('script'):
+            if script.get('src'):
+                script['src'] = urljoin(url, script['src'])
+        for img in soup.find_all('img'):
+            if img.get('src'):
+                img['src'] = urljoin(url, img['src'])
+
+        return render_template('preview.html', content=soup.prettify())
+    except Exception as e:
+        print(e)
+        return 'Failed to preview URL.'
 
 
 if __name__ == '__main__':
